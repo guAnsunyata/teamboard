b03a1ee6f4b1a66f121b1fcf0bdc12ab3c39826e
imrobinized <almigod0718@gmail.com>
2015-11-27 00:32:00 +0800

    todo schema useless update
    changed file:
    /api/model/todoModel.js

--- api/model/todoModel.js
+++ api/model/todoModel.js
@@ -3,7 +3,6 @@ var Schema = mongoose.Schema;
 var todoSchema = new Schema({
 	title: String,
 	content: String,
-	test: [{user: String}],
 	collabs: [ Schema.Types.ObjectId ],
 	finished: {
 		type: Boolean,
