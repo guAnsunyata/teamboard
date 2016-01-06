7ebf8b68880410db2b083d78bbcf59e8fbed1ed7
imrobinized <almigod0718@gmail.com>
2016-01-07 01:25:24 +0800

    udpate facebook app config

--- passport/auth.js
+++ passport/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '941223029266728',
-		'clientSecret': '0d9e4c2ceebcc24ae2b03f1e9a1cee01',
+		'clientID': '477388279107051',
+		'clientSecret': '0fdffb0059d2d5be12e1b45553409dc2',
 		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
 	}
 }
