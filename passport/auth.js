b99f2143b880c546ed660ec334b991f7cddb2a8b
imrobinized <almigod0718@gmail.com>
2016-01-07 14:08:27 +0800

    udpate facebook login config
    passport/auth.js

--- passport/auth.js
+++ passport/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '477388279107051',
 		'clientSecret': '0fdffb0059d2d5be12e1b45553409dc2',
-		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL': 'http://guanworkshop.ddns.net:3000/auth/facebook/callback'
 	}
 }
